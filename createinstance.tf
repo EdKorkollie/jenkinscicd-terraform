993af577aba167495bce2cecab56d8dfc8a15993
edkorkollie <korkollieed@gmail.com>
2024-11-18 11:26:40 -0500

    creating instances

--- createinstance.tf
+++ createinstance.tf
@@ -3,3 +3,39 @@ resource "aws_key_pair" "jenkins_keypair" {
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
+resource "aws_instance" "jenkinsServer" {
+  ami = lookup(var.JenkinsAMIS, var.AWS_REGION)
+
+  key_name = aws_key_pair.jenkins_keypair
+
+  instance_type = "t2.small"
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
+
+resource "aws_instance" "nexusServer" {
+  ami = lookup(var.NexusAMIS, var.AWS_REGION)
+
+  key_name = aws_key_pair.jenkins_keypair
+
+  instance_type = "t2.medium"
+
+  tags = {
+    Name = "nexus-server"
+  }
+}
+
+resource "aws_instance" "sonarServer" {
+  ami = lookup(var.SonarAMIS, var.AWS_REGION)
+
+  key_name = aws_key_pair.jenkins_keypair
+
+  instance_type = "t2.medium"
+
+  tags = {
+    Name = "sonar-server"
+  }
+}
+
